b9e8c0e12e558039b45529924acb21165539586d
Andrei Robson da Silva <silva.andrei@outlook.com.br>
2024-06-23 17:40:28 +0000

    Criação de projeto Maven multi modulos
    Signed-off-by: Andrei Robson da Silva <silva.andrei@outlook.com.br>

--- quick-start-maven/src/main/java/digitalinnovation/App.java
+++ quick-start-maven/src/main/java/digitalinnovation/App.java
@@ -6,8 +6,7 @@ package digitalinnovation;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public String mensagem(){
+        return "Olá, mundo!";
     }
 }
